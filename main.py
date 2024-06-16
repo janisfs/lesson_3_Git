75a88f93325673f2d02d01bf42d56990172b8c02
janisfs <janisfs@mail.ru>
2024-06-16 18:40:14 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра тир")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     pass
